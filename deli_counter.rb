de01a9fa74947feefad39bf33a9a291bd34216d5
ishmaelali33 <ishmaelali33@gmail.com>
2020-10-28 12:46:09 -0400

    commit tree 48da9098489973900f1a3daf097f25a8eb2be71e

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+katz_deli = ["Ada", "Grace", "Kent"]
+
+take_a_number(katz_deli, "Ada")
+puts "Welcome, Ada. You are number 1 in line."
+take_a_number(katz_deli, "Grace")
+puts "Welcome, Grace. You are number 2 in line."
+take_a_number(katz_deli, "Kent")
+puts "Welcome, Kent. You are number 3 in line."
+end
+
+line(katz_deli)
+puts ""The line is currently: 1. Ada 2. Grace 3. Kent""
+end
+
+now_serving(katz_deli)
+puts "Currently serving Ada."
+puts "Currently serving Grace"
+puts "Currently serving Kent."
+end
+end
